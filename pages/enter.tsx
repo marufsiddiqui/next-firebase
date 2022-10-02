cafbab500cbfaeb1b08f1eac9b6d8c8b74442254
Maruf Siddiqui <siddiquim@mediamarktsaturn.com>
2022-10-02 12:15:16 +0200

    Add prettier

---
+++ pages/enter.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const EnterPage: NextPage = () => {
+  return (
+    <div>
+      <h1>EnterPage</h1>
+    </div>
+  );
+};
+
+export default EnterPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      Index
-    </div>
-  )
-}
+  return <div>Index</div>;
+};
 
-export default Home
+export default Home;
